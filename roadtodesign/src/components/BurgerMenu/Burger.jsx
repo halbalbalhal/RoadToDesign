60d4bbab4d32ab7264a0cd9f607ce03625b1a4fe
halbalbalhal <sanyabirulya@mail.ru>
2023-07-20 18:58:14 +0300

    added cross in burger menu

--- roadtodesign/src/components/BurgerMenu/Burger.jsx
+++ roadtodesign/src/components/BurgerMenu/Burger.jsx
@@ -1,19 +1,34 @@
 import styles from './Burger.module.css'
+import Cross from '../../Assets/images/Main/cross.svg'
 
-const BurgerMenu = () => {    
+const BurgerMenu = () => {  
+    
+    const BurgerClose = () => {
+        document.getElementById("bm").style.display = 'none'
+    }
+    
     return (
         <section className={styles.burger__menu} id="bm">
-
             <div className={styles.burger__black}>
+                <img src={Cross} alt="didn't find" className={styles.burger__cross} onClick={BurgerClose} />
                 <ul>
-                    <li>Преимущества</li>
-                    <li>Отзывы</li>
-                    <li>О нас</li>
-                    <li>Заказать превью</li>
+                    <li onClick={() => {
+                        window.location.href = '#benefits'
+                        document.getElementById("bm").style.display = 'none'
+                    }}>Преимущества</li>
+                    <li onClick={() => {
+                        window.location.href = '#reviews'
+                        document.getElementById("bm").style.display = 'none'
+                    }}>Отзывы</li>
+                    <li onClick={() => {
+                        window.location.href = '#about'
+                        document.getElementById("bm").style.display = 'none'                        
+                    }}>О нас</li>
+                    <li onClick={() => {
+                        window.location.href = '#order'
+                        document.getElementById("bm").style.display = 'none'
+                    }}>Заказать превью</li>
                 </ul>
-                <button id='button' onClick={() => {
-                    document.getElementById("bm").style.display = 'none'
-                }}>Close</button>
             </div>
         </section>
     )
